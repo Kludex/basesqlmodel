84a5c503933e1287150d12161445fba25fab34a0
Marcelo Trylesinski <marcelotryle@gmail.com>
2022-01-16 12:12:53 +0100

    Add `load_strategy` parameter on `get` and `get_multi` methods (#5)
    * Add `load_strategy` parameter on `get` and `get_multi` methods
    
    Co-authored-by: Nicolas Chaulet <nicolas.chaulet@gmail.com>
    
    * Fix linter
    
    * Add tests for get_multi
    
    Co-authored-by: Nicolas Chaulet <nicolas.chaulet@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,8 +1,9 @@
 import pytest
+from sqlalchemy.exc import InvalidRequestError
 from sqlmodel.ext.asyncio.session import AsyncSession
 
 from basesqlmodel.main import Base, InvalidTable, is_table
-from tests.utils import Potato
+from tests.utils import Potato, Potatoes
 
 
 def test_is_table() -> None:
@@ -42,3 +43,99 @@ async def test_update(session: AsyncSession) -> None:
 async def test_delete(session: AsyncSession) -> None:
     obj = await Potato.create(session, name="Blue")
     assert await Potato.delete(session, name="Blue") == obj
+
+
+async def test_eagerload(session: AsyncSession) -> None:
+    collection = await Potatoes.create(session)
+    await Potato.create(session, name="Blue", potato_collection_id=collection.id)
+    await Potato.create(session, name="Red", potato_collection_id=collection.id)
+
+    potatoes = await Potatoes.get(session, load_strategy={"potatoes": "selectin"})
+    assert len(potatoes.potatoes) == 2
+
+
+async def test_subqueryload(session: AsyncSession) -> None:
+    collection = await Potatoes.create(session)
+    await Potato.create(session, name="Blue", potato_collection_id=collection.id)
+    await Potato.create(session, name="Red", potato_collection_id=collection.id)
+
+    potatoes = await Potatoes.get(session, load_strategy={"potatoes": "subquery"})
+    assert len(potatoes.potatoes) == 2
+
+
+@pytest.mark.parametrize("strategy", ("raise", "raise_on_sql"))
+async def test_raiseload(strategy: str, session: AsyncSession) -> None:
+    collection = await Potatoes.create(session)
+    await Potato.create(session, name="Blue", potato_collection_id=collection.id)
+    await Potato.create(session, name="Red", potato_collection_id=collection.id)
+
+    potatoes = await Potatoes.get(session, load_strategy={"potatoes": strategy})
+    with pytest.raises(InvalidRequestError):
+        potatoes.potatoes
+
+
+async def test_noload(session: AsyncSession) -> None:
+    collection = await Potatoes.create(session)
+    await Potato.create(session, name="Blue", potato_collection_id=collection.id)
+    await Potato.create(session, name="Red", potato_collection_id=collection.id)
+
+    potatoes = await Potatoes.get(session, load_strategy={"potatoes": "noload"})
+    assert len(potatoes.potatoes) == 0
+
+
+async def test_eagerload_multi(session: AsyncSession) -> None:
+    first = await Potatoes.create(session, name="First")
+    second = await Potatoes.create(session, name="Second")
+
+    await Potato.create(session, name="Blue", potato_collection_id=first.id)
+    await Potato.create(session, name="Red", potato_collection_id=first.id)
+
+    await Potato.create(session, name="Green", potato_collection_id=second.id)
+    await Potato.create(session, name="Yellow", potato_collection_id=second.id)
+
+    potatoes = await Potatoes.get_multi(session, load_strategy={"potatoes": "selectin"})
+    assert [len(potato.potatoes) for potato in potatoes] == [2, 2]
+
+
+async def test_subqueryload_multi(session: AsyncSession) -> None:
+    first = await Potatoes.create(session, name="First")
+    second = await Potatoes.create(session, name="Second")
+
+    await Potato.create(session, name="Blue", potato_collection_id=first.id)
+    await Potato.create(session, name="Red", potato_collection_id=first.id)
+
+    await Potato.create(session, name="Green", potato_collection_id=second.id)
+    await Potato.create(session, name="Yellow", potato_collection_id=second.id)
+
+    potatoes = await Potatoes.get_multi(session, load_strategy={"potatoes": "subquery"})
+    assert [len(potato.potatoes) for potato in potatoes] == [2, 2]
+
+
+@pytest.mark.parametrize("strategy", ("raise", "raise_on_sql"))
+async def test_raiseload_multi(strategy: str, session: AsyncSession) -> None:
+    first = await Potatoes.create(session, name="First")
+    second = await Potatoes.create(session, name="Second")
+
+    await Potato.create(session, name="Blue", potato_collection_id=first.id)
+    await Potato.create(session, name="Red", potato_collection_id=first.id)
+
+    await Potato.create(session, name="Green", potato_collection_id=second.id)
+    await Potato.create(session, name="Yellow", potato_collection_id=second.id)
+
+    potatoes = await Potatoes.get_multi(session, load_strategy={"potatoes": strategy})
+    with pytest.raises(InvalidRequestError):
+        [potato.potatoes for potato in potatoes]
+
+
+async def test_noload_multi(session: AsyncSession) -> None:
+    first = await Potatoes.create(session, name="First")
+    second = await Potatoes.create(session, name="Second")
+
+    await Potato.create(session, name="Blue", potato_collection_id=first.id)
+    await Potato.create(session, name="Red", potato_collection_id=first.id)
+
+    await Potato.create(session, name="Green", potato_collection_id=second.id)
+    await Potato.create(session, name="Yellow", potato_collection_id=second.id)
+
+    potatoes = await Potatoes.get_multi(session, load_strategy={"potatoes": "noload"})
+    assert [len(potato.potatoes) for potato in potatoes] == [0, 0]
